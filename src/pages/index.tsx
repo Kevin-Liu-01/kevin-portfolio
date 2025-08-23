1b1a060881ac0f2d7cc2ab73bf335f132ef7fe76
Kevin Liu <kk23907751@gmail.com>
2025-08-23 01:42:18 -0400

    [Feat] Move projects into index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,230 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 // import { api } from "~/utils/api";
-import { ChatIcon, MailIcon, CursorClickIcon } from "@heroicons/react/solid";
+import {
+  ChatIcon,
+  MailIcon,
+  CursorClickIcon,
+  ArchiveIcon,
+  ArrowsExpandIcon,
+  BookmarkAltIcon,
+  CalculatorIcon,
+  ChipIcon,
+  CodeIcon,
+  CreditCardIcon,
+  DocumentSearchIcon,
+  EyeIcon,
+  IdentificationIcon,
+  LibraryIcon,
+  LightBulbIcon,
+  MicrophoneIcon,
+  PaperClipIcon,
+  PuzzleIcon,
+  ScaleIcon,
+  ServerIcon,
+  SparklesIcon,
+  TerminalIcon,
+  TrashIcon,
+  VariableIcon,
+} from "@heroicons/react/solid";
 import Navbar from "./components/navbar";
 import Chat from "./components/chatgpt";
-import { projects } from "./data/projects";
+
+const projects = [
+  {
+    name: "Lumachor",
+    url: "https://lumachor.vercel.app/home",
+    description:
+      "Lumachor is a context engine that gives every user the power of an expert prompt engineer by automatically injecting the perfect background knowledge into any conversation.",
+    image: "/lumachor.png",
+    icon: <ArrowsExpandIcon />,
+  },
+  {
+    name: "HackPrinceton 2025S",
+    url: "https://hack-princeton-fall-2025-demo.vercel.app/",
+    description:
+      "Main website/landing page for HackPrinceton Fall 2025, Princeton's premier hackathon.",
+    image: "/hackprinceton25f.png",
+    icon: <CodeIcon />,
+  },
+  {
+    name: "Splitway",
+    url: "https://splitway.vercel.app/",
+    description: "Track expenses and split them with friends.",
+    image: "/splitway.png",
+    icon: <ArrowsExpandIcon />,
+  },
+  {
+    name: "Lootbox Simulator",
+    url: "https://lootboxsimulator.vercel.app/",
+    description:
+      "Simulator-style game where users can try opening different kinds of lootboxes.",
+    image: "/lootboxsimulator.png",
+    icon: <ArchiveIcon />,
+  },
+  {
+    name: "PawPointClicker",
+    url: "https://pawpointclicker.vercel.app/",
+    description:
+      "Cookie Clicker inspired game where you collect Princeton's Paw Points.",
+    image: "/pawpointclicker.png",
+    icon: <CreditCardIcon />,
+  },
+  {
+    name: "HackPrinceton 2025S",
+    url: "https://hack-princeton-spring-2025-demo.vercel.app/",
+    description:
+      "Main website/landing page for HackPrinceton Spring 2025, Princeton's premier hackathon.",
+    image: "/hackprinceton25s.png",
+    icon: <CodeIcon />,
+  },
+  {
+    name: "HackPrinceton 2024F",
+    url: "https://hack-princeton-fall-2024-demo.vercel.app/",
+    description:
+      "Main website/landing page for HackPrinceton Fall 2024, Princeton's premier hackathon.",
+    image: "/hackprinceton24f.png",
+    icon: <CodeIcon />,
+  },
+  {
+    name: "SnellTech",
+    url: "https://snelltech.vercel.app/",
+    description:
+      "Low-cost digital visual acuity exam using the Snellen Eye Chart.",
+    image: "/snelltech.png",
+    icon: <EyeIcon />,
+  },
+  {
+    name: "LetMeCook",
+    url: "https://letmecook.vercel.app/",
+    description: "Scans your refrigerator to generate recipes using ChatGPT.",
+    image: "/letmecook.png",
+    icon: <LightBulbIcon />,
+  },
+  {
+    name: "Balladeer",
+    url: "https://balladeer.vercel.app/",
+    description: "Generates full study guides for literary works.",
+    image: "/balladeer.jpg",
+    icon: <LibraryIcon />,
+  },
+  {
+    name: "CompassUSA",
+    url: "https://compass-usa.vercel.app/",
+    description: "A tool to help immigrants find support and resources.",
+    image: "/compassusa.jpg",
+    icon: <IdentificationIcon />,
+  },
+  {
+    name: "ApneaAlert",
+    url: "https://apnea-alert-git-main-kevin-liu-01.vercel.app/",
+    description:
+      "Apnea Alert is an affordable wearable sensor to help you sleep soundly.",
+    image: "/apnea-alert.png",
+    icon: <ChipIcon />,
+  },
+  {
+    name: "Iron Triangle",
+    url: "https://iron-triangle.vercel.app/",
+    description: "U.S. History II Final; Analyzes Military Industrial Complex.",
+    image: "/irontriangle.png",
+    icon: <ScaleIcon />,
+  },
+  {
+    name: "AdventureGPT",
+    url: "https://adventuregpt.vercel.app/",
+    description:
+      "Generates unique, exciting stories based on a user-inputted prompt.",
+    image: "/adventuregpt.png",
+    icon: <SparklesIcon />,
+  },
+  {
+    name: "EditorGPT",
+    url: "https://editorgpt.vercel.app/",
+    description: "A code editor that allows ChatGPT to review your code.",
+    image: "/editorgpt.png",
+    icon: <DocumentSearchIcon />,
+  },
+
+  {
+    name: "OMMC Portal",
+    url: "https://ommc-test-portal.vercel.app/",
+    description: "The official test portal of the OMMC competition.",
+    image: "/ommcportal.jpg",
+    icon: <CalculatorIcon />,
+  },
+  {
+    name: "OMMC Sample Portal",
+    url: "https://ommc-sample-portal.vercel.app/",
+    description: "The official sample test portal of OMMC.",
+    image: "/ommcsampleportal.png",
+    icon: <CalculatorIcon />,
+  },
+  {
+    name: "Enkrateia",
+    url: "https://enkrateia.vercel.app/",
+    description:
+      "An application that accesses the ChatGPT 3.5 and GPT-4 models using the OpenAI API",
+    image: "/enkrateia.png",
+    icon: <TerminalIcon />,
+  },
+  {
+    name: "HD Transcribe",
+    url: "https://hd-transcribe.vercel.app",
+    description:
+      "Accesses a novel speech model designed to understand patients with Huntington's Disease",
+    image: "/hd-transcribe.png",
+    icon: <MicrophoneIcon />,
+  },
+  {
+    name: "OMMC",
+    url: "https://www.ommcofficial.org",
+    description:
+      "The official website of the Online Monmouth Math Competition, an online math competition for students across the country.",
+    image: "/ommc.png",
+    icon: <VariableIcon />,
+  },
+  {
+    name: "OMMC Atlas",
+    url: "https://ommc-atlas.vercel.app/",
+    description:
+      "The fullstack database for all OMMC questions. Includes all Year 1 questions from 2020-2021.",
+    image: "/ommc-atlas.png",
+    icon: <ServerIcon />,
+  },
+  {
+    name: "RecyclAIble",
+    url: "https://recyclaible.vercel.app/",
+    description:
+      "Hackathon project to use computer vision for smart recycling. Won 1st in Hardware at PennApps XXIII!",
+    image: "/recyclaible.png",
+    icon: <TrashIcon />,
+  },
+  {
+    name: "PlantSTEM",
+    url: "https://plant-stem.vercel.app/",
+    description:
+      "A website to help students learn about subjects like Math and Physics, as well as Chess!",
+    image: "/plantstem.png",
+    icon: <BookmarkAltIcon />,
+  },
+  {
+    name: "Tutorial",
+    url: "https://tutorial-nu.vercel.app/",
+    description:
+      "An app to help tutors and pupils connect with each other and create listings.",
+    image: "/tutorial.png",
+    icon: <PaperClipIcon />,
+  },
+  {
+    name: "Satellite Crafter",
+    url: "https://satellite-crafter.vercel.app/",
+    description:
+      "My first app! A game to create satellites from certain parts.",
+    image: "/satellitecrafter.png",
+    icon: <PuzzleIcon />,
+  },
+];
 
 const Home: NextPage = () => {
   const [pattern, setPattern] = useState("cross");
